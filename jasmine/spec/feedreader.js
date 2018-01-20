d0dd43b1d3d4143ea59a36fe1cba789ff191d3f4
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2018-01-20 18:15:28 +0100

    test if menu is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -57,19 +57,25 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
+    /* new test suite named "The menu" */
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            var checkForClass = $('body').hasClass('menu-hidden')
+            expect(checkForClass).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
